3f4ec5c847aea288bd426963381dffa988f15dcd
Akhil Kumar <akillcoder@gmail.com>
2025-09-05 17:21:05 +0530

    feat: app1 docker file

--- apps/app1/main.js
+++ apps/app1/main.js
@@ -4,7 +4,7 @@ const randomLog = require('./random-log.util')
 const app = express()
 const PORT = process.env.PORT || 3000
 const GENERATE_LOGS_TIME_SECONDS = process.env.GENERATE_LOGS_TIME_SECONDS || 1
-const appName = 'app1'
+const APP_NAME = process.env.APP_NAME || 'app1'
 
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'OK' })
@@ -12,7 +12,7 @@ app.get('/health', (req, res) => {
 
 
 setInterval(() => {
-    const logMessage = randomLog(appName);
+    const logMessage = randomLog(APP_NAME);
 
     //TODO: publish this log to kafka
     console.log(logMessage);
